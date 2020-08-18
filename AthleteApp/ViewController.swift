c22b23d8b3862ad8e0c9fc22b69063482df5c181
Rohit Kumar <rohitkumar@code-brew.com>
2020-08-18 08:53:17 +0530

    Initial Commit

---
+++ AthleteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AthleteApp
+//
+//  Created by Rohit Prajapati on 18/08/20.
+//  Copyright © 2020 Rohit Prajapati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
